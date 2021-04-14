93c52d20638f5962a089d5778ccc81ef889f9a27
Wander Rodrigues <wander@writ.solutions>
2021-04-14 01:06:00 -0400

    Criar projeto do Angular
    Closes  #1

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    },
+    {
+      provide:  DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+  firebase: {
+    apiKey: "AIzaSyBwzSnzTmXtrGAGhG4BGTZUXshrppc2U3U",
+    authDomain: "wr-it-solutions.firebaseapp.com",
+    databaseURL: "https://wr-it-solutions.firebaseio.com",
+    projectId: "wr-it-solutions",
+    storageBucket: "wr-it-solutions.appspot.com",
+    messagingSenderId: "719793371031",
+    appId: "1:719793371031:web:3083ac0801c092e1edb6d2",
+    measurementId: "G-CT22CL9S98"
+  }
